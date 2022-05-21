fd91f1edfeeafa21092207fc7ddb9ddf91237ae2
Andre Castellanos <47199821+acastellanos95@users.noreply.github.com>
2022-05-20 23:02:22 -0500

    HW update

--- (1+1)-EE.h
+++ (1+1)-EE.h
@@ -10,12 +10,23 @@
 #include <fstream>
 #include "rand.h"
 
+/// Función a usar
+/// \param x : punto x
+/// \return f(x)
 double function(const std::vector<double> &x){
   // f(x_1,x_2) = (4 - 2.1*x_1^2 + (x_1^4)/3)*x_1^2 + x_1*x_2 + (-4 + 4x_2^2)x_2^2
   return (4.0 - 2.1*x[0]*x[0] + ((x[0]*x[0]*x[0]*x[0])/3.0))*x[0]*x[0] + x[0]*x[1] + (-4.0 + 4.0*x[1]*x[1])*x[1]*x[1];
 }
 
-void onePOneEE(int Gmax, std::vector<double> &x, const std::vector<std::pair<double, double>> &constraints){
+/// Buscar el óptimo dado un punto x de inicio, máximo de generaciones Gmax y restricciones tipo a_i <= x_i <= b_i en un arreglo constraints
+/// \param Gmax : Número máximo de generaciones
+/// \param x : Punto inicializado (número de variables n = x.size())
+/// \param constraints : arreglo de restricciones tipo a_i <= x_i <= b_i {(a_1,b_1),...,(a_n,b_n)}
+/// \param filename : nombre de archivo donde guardaremos
+/// \return arreglo (x,f(x))
+std::vector<std::pair<std::vector<double>, double>> onePOneEE(int Gmax, std::vector<double> &x, const std::vector<std::pair<double, double>> &constraints, std::string filename){
+  std::vector<std::pair<std::vector<double>, double>> results;
+
   // Checar errores
   if(x.size() != constraints.size())
     throw std::runtime_error("longitud del punto x no coincide con el número de restricciones");
@@ -27,14 +38,16 @@ void onePOneEE(int Gmax, std::vector<double> &x, const std::vector<std::pair<dou
   std::vector<double> sigma(Gmax + 2, 0.0);
   sigma[0] = 3.0;
   double c = 0.817;
-  std::ofstream file("out3.txt");
+  std::ofstream file(filename);
+  results.emplace_back(x,f);
 
-  // Imprimir al archivo inicialmente
+  // Impresión inicial
+  // Imprimir al archivo
   file << "x=(";
   for(size_t index = 0; index < x.size(); ++index){
     file << std::to_string(x[index]) << ", "[index == x.size() - 1];
   }
-  file << "), f(x)=" << std::to_string(f) << ", ps=" << std::to_string(ps) << '\n';
+  file << "), f(x)=" << std::to_string(f) << '\n';
 
   for (int t = 0; t <= Gmax;) {
     // Inicializar semilla e hijo
@@ -44,7 +57,7 @@ void onePOneEE(int Gmax, std::vector<double> &x, const std::vector<std::pair<dou
     // Mutar al vector x
     for(size_t i = 0; i < xPrime.size(); ++i){
       xPrime[i] = x[i] + sigma[t] * N(0,1);
-      // Seguir buscando x'_i que satisfaga las restricciones
+      // Seguir buscando x'_i que satisfaga las restricciones si no las cumplió anteriormente
       while(constraints[i].second < xPrime[i] || xPrime[i] < constraints[i].first){
         xPrime[i] = x[i] + sigma[t] * N(0,1);
       }
@@ -54,21 +67,22 @@ void onePOneEE(int Gmax, std::vector<double> &x, const std::vector<std::pair<dou
     double Fprime = function(xPrime);
     f = function(x);
     if(Fprime < f) {
+      // Hijo reemplaza al padre y aumenta el contador de número de hijos exitosos
       x = xPrime;
       f = Fprime;
 
-      // Hijo reemplaza al padre
       freq += 1.0;
     }
 
-    // Imprimir al archivo
+    // Imprimir al archivo y agregar a resultados
     file << "x=(";
     for(size_t index = 0; index < x.size(); ++index){
       file << std::to_string(x[index]) << ", "[index == x.size() - 1];
     }
-    file << "), f(x)=" << std::to_string(f) << ", ps=" << std::to_string(ps) << '\n';
+    file << "), f(x)=" << std::to_string(f) << '\n';
+    results.emplace_back(x,f);
 
-    // Modificar sigma[t]
+    // t = t+1 y modificamos sigma[t]
     ++t;
     if(t%x.size() == 0){
       if(ps > 1.0/5.0){
@@ -82,14 +96,15 @@ void onePOneEE(int Gmax, std::vector<double> &x, const std::vector<std::pair<dou
       sigma[t] = sigma[t-1];
     }
 
-    // Modificación de ps (aumentamos t en uno porque empezamos desde 0 que es la iteración 1, así sucesivamente)
+    // Modificación de ps
     if(t%(10*x.size()) == 0){
-      ps = freq / 10.0*x.size();
+      ps = freq / (10.0*x.size());
       freq = 0.0;
     }
   }
 
   file.close();
+  return results;
 }
 
 #endif //_1_1__EE__1_1_EE_H_
--- main.cpp
+++ main.cpp
@@ -1,13 +1,64 @@
 #include <iostream>
-#include "rand.h"
+#include <random>
 #include "(1+1)-EE.h"
 
 int main() {
-  int Gmax = 20000;
-  // (1.0,1.5) => x=(-0.089842,0.712656 ), f(x)=-1.031628 20000
-  // (-3.0,2.0) => x=(0.089842,-0.712656 ), f(x)=-1.031628, ps=0.000000
-  std::vector<double> x{1.0,1.0};
+  int Gmax = 500;
+  // (1.0,1.5) => x=(-0.089842,0.712656 ), f(x)=-1.031628 20000 lo mismo con (-1,1)
+  // (-3.0,2.0) => x=(0.089842,-0.712656 ), f(x)=-1.031628, ps=0.000000 lo mismo con (1,1)
+  // Probé con (1.0,1.5)
+  std::vector<double> x{-3.0,2.0};
   const std::vector<std::pair<double, double>> constraints{std::make_pair(-3.0,3.0), std::make_pair(-2.0,2.0)};
-  onePOneEE(Gmax, x, constraints);
+  auto results = onePOneEE(Gmax, x, constraints, "(-3,2)Prueba.txt");
+
+  // Crear reporte de estadisticas de 20 corridas
+  /*std::ofstream report("report.txt");
+  report << "Ejecución & $x_1$ & $x_2$ & $f(x)$ & Media & Varianza & Desviación estandar & Mejor & Peor & Gmax\\\\\n";
+  std::random_device rd;  // Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<double> disX(constraints[0].first, constraints[0].second);
+  std::uniform_real_distribution<double> disY(constraints[1].first, constraints[1].second);
+  for (int i = 0; i < 20; ++i) {
+
+    // Generamos x y lo gurdamos ya que onePOneEE pasa valores por referencia
+    x[0] = disX(gen);
+    x[1] = disY(gen);
+    auto xPrime = x;
+
+    // f(x)
+    double f = function(x);
+
+    // Corremos estrategia evolutiva
+    auto result = onePOneEE(Gmax, x, constraints, "reporte"+std::to_string(i+1)+".txt");
+
+    // Media, min y max (mejor y peor)
+    double avg = 0.0;
+    double min = std::numeric_limits<double>::max();
+    double max = std::numeric_limits<double>::min();
+    for(auto &element: result){
+      // buscar mínimo
+      if(element.second < min){
+        min = element.second;
+      }
+      // buscar máximo
+      if(element.second > max){
+        max = element.second;
+      }
+
+      avg += element.second;
+    }
+    avg /= (double) result.size();
+
+    // Varianza y Desviación estandar
+    double s2 = 0.0;
+    for(auto &element: result){
+      s2 += (element.second - avg)*(element.second - avg);
+    }
+    s2 /= (double) result.size();
+    double dev = sqrt(s2);
+    report << i+1 << " & " << xPrime[0] << " & " << xPrime[1] << " & $" << std::to_string(f) << "$ & $" << std::to_string(avg) << "$ & $" << std::to_string(s2) << "$ & $" << std::to_string(dev) << "$ & $" << std::to_string(min) << "$ & $" << std::to_string(max) << "$ & $" << Gmax << "$\\\\\n";
+  }
+  report.close();*/
+
   return 0;
 }
